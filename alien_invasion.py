636d92641e44fca2b9d4191496ddd2baa7424294
Amena Mabrouk <mabrouk.amena@gmail.com>
2023-06-02 15:14:41 -0500

    fix code to event.key

--- alien_invasion.py
+++ alien_invasion.py
@@ -38,7 +38,7 @@ class AlienInvasion:
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = True
-                elif event.type == pygame.K_LEFT:
+                elif event.key == pygame.K_LEFT:
                     self.ship.moving_left = True
             
             elif event.type == pygame.KEYUP:
